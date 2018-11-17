d3dbf1f424c0432c2214fe7352fb2cd4e77cec43
tdesosa <tdesosa@gmail.com>
2018-11-17 16:27:55 -0700

    new post form implemented

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
@@ -12,4 +13,12 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form is_valid():
+            post = form.save
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
 
